24a9c9fd10bf5e61be263b761f0b6965032d464a
Jiahao Liu <jliu@astrata.eu>
2014-08-27 16:43:37 +0200

    Locked the screen orientation. Set a button to initialize the camera

--- app/src/main/java/com/jiahaoliuliu/simplecamapp/SimpleCamAppActivity.java
+++ app/src/main/java/com/jiahaoliuliu/simplecamapp/SimpleCamAppActivity.java
@@ -1,14 +1,31 @@
 package com.jiahaoliuliu.simplecamapp;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SimpleCamAppActivity extends ActionBarActivity {
 
+    private Button startCameraButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Lock the screen orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         setContentView(R.layout.activity_simple_cam_app);
+
+        startCameraButton = (Button)findViewById(R.id.startCameraButton);
+        startCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
