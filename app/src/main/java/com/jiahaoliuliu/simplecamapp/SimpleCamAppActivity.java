fa0bb6f9e25316f1785fc730983174436eb55a9e
Jiahao Liu <jliu@astrata.eu>
2014-08-27 16:37:27 +0200

    Created the first version of the app

---
+++ app/src/main/java/com/jiahaoliuliu/simplecamapp/SimpleCamAppActivity.java
@@ -0,0 +1,14 @@
+package com.jiahaoliuliu.simplecamapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class SimpleCamAppActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_cam_app);
+    }
+}
