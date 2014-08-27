0af99982b50d35233c99007c4c54f8866f613e6d
Jiahao Liu <jliu@astrata.eu>
2014-08-27 16:50:05 +0200

    Added the checker for camera hardware

--- app/src/main/java/com/jiahaoliuliu/simplecamapp/SimpleCamAppActivity.java
+++ app/src/main/java/com/jiahaoliuliu/simplecamapp/SimpleCamAppActivity.java
@@ -1,14 +1,18 @@
 package com.jiahaoliuliu.simplecamapp;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class SimpleCamAppActivity extends ActionBarActivity {
 
+    private Context context;
     private Button startCameraButton;
 
     @Override
@@ -20,11 +24,18 @@ public class SimpleCamAppActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_simple_cam_app);
 
+        context = this;
+
         startCameraButton = (Button)findViewById(R.id.startCameraButton);
+
         startCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // If the device does not have camera, do not anything
+                if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+                    Toast.makeText(context, R.string.error_message_no_camera_detected, Toast.LENGTH_LONG).show();
+                    return;
+                }
             }
         });
     }
