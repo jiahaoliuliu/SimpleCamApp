a9cffbbda0ed842e54a47b4967e1fe8468966230
Jiahao Liu <jliu@astrata.eu>
2014-08-28 10:39:01 +0200

    Created the file where the picture taken is saved. It is under DCIM directory

--- app/src/main/java/com/jiahaoliuliu/simplecamapp/SimpleCamAppActivity.java
+++ app/src/main/java/com/jiahaoliuliu/simplecamapp/SimpleCamAppActivity.java
@@ -5,16 +5,30 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+
+/***
+ * Sample class with the implementation provided by the developer portal of Android:
+ * http://developer.android.com/training/camera/photobasics.html
+ */
 public class SimpleCamAppActivity extends ActionBarActivity {
+
+    private static final String TAG = "SimpleCamAppActivity";
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -23,6 +37,11 @@ public class SimpleCamAppActivity extends ActionBarActivity {
     // Layout
     private Button mStartCameraButton;
     private ImageView mPhotoImageView;
+
+    // Other data
+    private String mCurrentPhotoPath;
+    private boolean mExternalStorageAvailable;
+    private boolean mExternalStorageWriteable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +60,7 @@ public class SimpleCamAppActivity extends ActionBarActivity {
         mStartCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 // If the device does not have camera, do not anything
                 if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
                     Toast.makeText(mContext, R.string.error_message_no_camera_detected, Toast.LENGTH_LONG).show();
@@ -52,7 +72,18 @@ public class SimpleCamAppActivity extends ActionBarActivity {
                 // This will prevent the crash when the app is calling the startActivityForResult
                 // when no app can handle it
                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    try {
+                        // Create the File where the photo should go
+                        File photoFile = createImageFile();
+                        if (photoFile != null) {
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                        }
+                    } catch (IOException ex) {
+                        // Error occurred while creating the file
+                        Log.e(TAG, "Error while creating a new file in the external public storage", ex);
+                    }
+
                 }
             }
         });
@@ -61,10 +92,52 @@ public class SimpleCamAppActivity extends ActionBarActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitMap = (Bitmap) extras.get("data");
-            mPhotoImageView.setImageBitmap(imageBitMap);
-            mPhotoImageView.setVisibility(View.VISIBLE);
+            // Show the image if it is possible
+            if (data != null) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitMap = (Bitmap) extras.get("data");
+                mPhotoImageView.setImageBitmap(imageBitMap);
+                mPhotoImageView.setVisibility(View.VISIBLE);
+            }
+
+            // Add the image to the gallery
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            File file = new File(mCurrentPhotoPath);
+            Uri contentUri = Uri.fromFile(file);
+            mediaScanIntent.setData(contentUri);
+            this.sendBroadcast(mediaScanIntent);
         }
+    }
+
+    private File createImageFile() throws IOException {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            mExternalStorageAvailable = mExternalStorageWriteable = true;
+            Log.v(TAG, "The external storage is available and writable");
+        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            mExternalStorageAvailable = true;
+            mExternalStorageWriteable = false;
+            Log.v(TAG, "The external storage is available but not writable");
+        } else {
+            mExternalStorageAvailable = mExternalStorageWriteable = false;
+            Log.v(TAG, "The external storage is not available nor writable");
+        }
+
+        if (!mExternalStorageAvailable || !mExternalStorageWriteable) {
+            return null;
+        }
+
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+
+        // Make sure that the external storage exists
+        storageDir.mkdir();
+        File imagetmp = new File(storageDir, imageFileName+".jpg");
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + imagetmp.getAbsolutePath();
+        return imagetmp;
     }
 }
